9676813901759260df1e1550f3935f5ad92ce541
Maksim Potehin <m.potehin@softonix.org>
2024-10-21 19:17:45 +0300

    - calculator in progress

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,48 @@
 <template>
-  <div class="flex items-center w-full gap-x-6 px-5 py-3">
-    <div class="logo w-12 h-12" />
-    <div class="flex flex-grow gap-x-3">
-      <NuxtLink
-        v-for="item in navigation"
-        :key="item.label"
-        :to="{ name: item.routeName }"
-        :class="[
-          'hover:bg-gray-600/60 flex items-center px-4 py-2 text-sm font-medium rounded-md transition-all'
-        ]"
-        active-class="bg-gray-600 text-white"
-      >
-        {{ item.label }}
-      </NuxtLink>
-    </div>
-
-    <client-only>
-      <div class="flex gap-x-4 w-[300px]">
-        <el-input v-model="search" />
-
-        <div class="flex flex-col">
-          <AppIconGbFlag
-            class="w-4 hover:brightness-90 cursor-pointer transition-all"
-            :class="locale === locales[0].code ? 'brightness-100' : 'brightness-50'"
-            @click="locale = locales[0].code"
-          />
-          <AppIconUkraineFlag
-            class="w-4 hover:brightness-90 cursor-pointer transition-all"
-            :class="locale === locales[1].code ? 'brightness-100' : 'brightness-50'"
-            @click="locale = locales[1].code"
-          />
+  <div class="flex flex-col h-full overflow-hidden">
+    <header class="gap-x-6 shadow-[0px_1px_0px_0px_#2e3448]">
+      <div class="w-full max-w-[1440px] flex items-center m-auto px-5 py-3">
+        <div class="logo w-12 h-12" />
+        <div class="flex flex-grow gap-x-3">
+          <NuxtLink
+            v-for="item in navigation"
+            :key="item.label"
+            :to="{ name: item.routeName }"
+            :class="[
+              'hover:bg-gray-600/60 flex items-center px-4 py-2 text-sm rounded-md transition-all'
+            ]"
+            active-class="bg-gray-600 text-white"
+          >
+            {{ item.label }}
+          </NuxtLink>
         </div>
+
+        <client-only>
+          <div class="flex gap-x-4 w-[300px]">
+            <el-input v-model="search" />
+
+            <div class="flex flex-col">
+              <AppIconGbFlag
+                class="w-4 hover:brightness-90 cursor-pointer transition-all"
+                :class="locale === locales[0].code ? 'brightness-100' : 'brightness-50'"
+                @click="locale = locales[0].code"
+              />
+
+              <AppIconUkraineFlag
+                class="w-4 hover:brightness-90 cursor-pointer transition-all"
+                :class="locale === locales[1].code ? 'brightness-100' : 'brightness-50'"
+                @click="locale = locales[1].code"
+              />
+            </div>
+          </div>
+        </client-only>
       </div>
-    </client-only>
-  </div>
-  <div class="flex h-full bg-gray-800">
-    <div class="flex-grow flex flex-col">
+    </header>
+
+    <div class="max-w-[1440px] w-full flex flex-col flex-grow overflow-auto m-auto p-5">
       <!-- MAIN -->
-      <main class="flex-grow flex w-full h-full p-5">
-        <slot class="bg-gray-600" />
+      <main class="flex w-full flex-grow overflow-auto bg-gray-800 p-5 rounded-md">
+        <slot />
       </main>
     </div>
   </div>
--- pages/calculator/index.vue
+++ pages/calculator/index.vue
@@ -1,6 +1,59 @@
 <template>
-  <div>
-    <p> Calculator </p>
+  <div сlass="flex-grow">
+    <div class="mb-8">
+      <p class="text-warning text-2xl mb-4">Інвестиційний калькулятор</p>
+      <p class="text-white-400 leading-7">
+        Розраховуйте прибуток із врахуванням складних відсотків
+        Цей калькулятор допоможе вам легко розрахувати зростання капіталу, показати динаміку на графіку та порівняти
+        інвестиційні стратегії для досягнення найвищого результату.
+      </p>
+    </div>
+
+    <div class="">
+      <p class="mb-4">Введіть данні для розрухунків:</p>
+      <div class="max-w-[300px] border-r border-gray-500">
+        <el-form
+          label-position="top"
+          class="w-full max-w-[250px]"
+        >
+          <el-form-item class="mb-3" label="Реінвестування ">
+            <el-switch
+              v-model="formModel.reinvestment"
+              style="--el-switch-on-color: #13ce66; --el-switch-off-color: #adb5bd"
+            />
+          </el-form-item>
+
+          <el-form-item class="flex-grow mb-3" label="Початковий депозит">
+            <el-input v-model.number="formModel.start" />
+          </el-form-item>
+
+          <el-form-item class="flex-grow mb-3" :label="depositLabel + ' внесок'">
+            <el-input v-model.number="formModel.deposit" class="mb-2" />
+            <el-radio-group v-model="formModel.frequency">
+              <el-radio-button label="weekly">Щотижня</el-radio-button>
+              <el-radio-button label="monthly">Щомісяця</el-radio-button>
+            </el-radio-group>
+          </el-form-item>
+
+          <el-form-item class="flex-grow mb-3" label="Відсоткова ставка">
+            <el-input v-model.number="formModel.percent" />
+          </el-form-item>
+
+          <el-form-item class="flex-grow mb-3" label="Тривалість">
+            <el-input v-model.number="formModel.duration" class="mb-2" />
+            <el-radio-group v-model="formModel.durationUnit">
+              <el-radio-button label="weeks">Тижні</el-radio-button>
+              <el-radio-button label="months">Місяці</el-radio-button>
+              <el-radio-button label="years">Роки</el-radio-button>
+            </el-radio-group>
+          </el-form-item>
+
+          <el-form-item class="flex-grow" label="Інфляція">
+            <el-input v-model.number="formModel.inflation" class="mb-2" />
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,5 +63,18 @@ definePageMeta({
   pageLabel: 'Calculator',
   navOrder: 1
 })
+
+const formModel = ref({
+  start: 0,
+  deposit: 0,
+  percent: 0,
+  duration: 0,
+  frequency: '',
+  durationUnit: '',
+  inflation: '',
+  reinvestment: true
+})
+
+const depositLabel = computed(() => formModel.value.frequency === 'weekly' ? 'Щотижневий' : 'Щомісячний')
 
 </script>
